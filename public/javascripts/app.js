1ddadf21d44e38fb33fbf177adc94d47a114e75c
Alex <Alexander.bar@gmail.com>
2014-08-14 16:31:06 +0300

    Changed to ui-routers

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,11 +2,31 @@
  * Created by Alex on 08/08/2014.
  */
 
-var app = angular.module("dungeon-world-game-master", ['ui.bootstrap']);
+var app = angular.module("dungeon-world-game-master", ['ui.bootstrap', 'ui.router']);
 
-app.controller('mainCtrl', function($scope){
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('Maps', {
+            url: "/Maps",
+            templateUrl: "templates/Maps.html"
+        })
+        .state('Chars', {
+            url: "/Chars",
+            templateUrl: "templates/Characters.html"
+        })
+        .state('Moves', {
+            url: "/Moves",
+            templateUrl: "templates/Moves.html"
+        })
+        .state('Plots', {
+            url: "/Plots",
+            templateUrl: "templates/Plots.html"
+        })
+        .state('Encounters', {
+            url: "/Encounters",
+            templateUrl: "templates/Encounters.html"
+        })                              ;
+});
+app.controller('mainCtrl', function ($scope) {
 
-
-    var i=0;
-    alert(i);
 });

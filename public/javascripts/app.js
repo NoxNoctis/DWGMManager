efa7a237dc338f40f32f73be233451ebd47e1525
Alex <Alexander.bar@gmail.com>
2014-08-14 11:40:07 +0300

    first

---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alex on 08/08/2014.
+ */
+
+var app = angular.module("dungeon-world-game-master", ['ui.bootstrap']);
+
+app.controller('mainCtrl', function($scope){
+
+
+    var i=0;
+    alert(i);
+});

c22cf5014077051fa8c847132e865cfb15f2e0d7
Alex <Alexander.bar@gmail.com>
2014-08-21 14:18:05 +0300

    ReAdded mapCtrl

---
+++ public/javascripts/mapCtrl.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alex on 21/08/2014.
+ */
+/**
+ * Created by Alex on 14/08/2014.
+ */
+
+app.controller('mapCtrl', function ($scope) {
+
+    $scope.currentMap = 'images/faerun.jpg';
+
+});

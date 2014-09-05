62a93213d0f24c542e8d268131dc545e7ea9b421
Alex <Alexander.bar@gmail.com>
2014-09-05 18:22:09 +0300

    Added more functionality to Cahrs

--- public/javascripts/charsCtrl.js
+++ public/javascripts/charsCtrl.js
@@ -5,74 +5,83 @@
 app.controller('charsCtrl', function ($scope, $dataCollector) {
 
     $scope.PCs = $dataCollector.getPCs();
-    $scope.debilities=$dataCollector.getDebilities();
+    $scope.debilities = $dataCollector.getDebilities();
 
-    $scope.getMaxHp=function(char){
-        var CON = _.find(char.stats,function(stat){
+    $scope.getMaxHp = function (char) {
+        var CON = _.find(char.stats, function (stat) {
             return stat.name == 'CON';
         });
-            return char.class.baseHP + CON.value;
+        return char.class.baseHP + CON.value;
     };
 
-    $scope.getMaxLoad=function(char){
-        var STR = _.find(char.stats,function(stat){
+    $scope.getMaxLoad = function (char) {
+        var STR = _.find(char.stats, function (stat) {
             return stat.name == 'STR';
         });
         return char.class.baseMaxLoad + $scope.getModifier(STR);
     };
 
-    $scope.getModifier=function(stat, debility){
-        var mod = debility?-1:0;
-        if(stat < 9)
-            return -1+mod;
-        else if(stat < 13)
-            return 0+mod;
-        else if(stat < 16)
-            return 1+mod;
-        else if(stat < 18)
-            return 2+mod;
+    $scope.getModifier = function (stat, debility) {
+        var mod = debility ? -1 : 0;
+        if (stat < 9)
+            return -1 + mod;
+        else if (stat < 13)
+            return 0 + mod;
+        else if (stat < 16)
+            return 1 + mod;
+        else if (stat < 18)
+            return 2 + mod;
         else
-            return 3+mod;
+            return 3 + mod;
     };
-    $scope.getLevelByXp=function(xp){
-        var lvl=1;
-        while(xp>=(lvl+7)){
-            xp -=(lvl+7);
+    $scope.getLevelByXp = function (xp) {
+        var lvl = 1;
+        while (xp >= (lvl + 7)) {
+            xp -= (lvl + 7);
             lvl++;
         }
         return lvl;
     };
-    $scope.getRemainderXp=function(xp){
-        var lvl=1;
-        while(xp>=(lvl+7)){
-            xp -=(lvl+7);
+    $scope.getRemainderXp = function (xp) {
+        var lvl = 1;
+        while (xp >= (lvl + 7)) {
+            xp -= (lvl + 7);
             lvl++;
         }
-        return (lvl+7 -xp);
+        return (lvl + 7 - xp);
     };
-    $scope.totalWeight=function(items){
-        var res=0;
-        angular.forEach(items, function(item){
-           res+=item.weight;
+    $scope.totalWeight = function (items) {
+        var res = 0;
+        angular.forEach(items, function (item) {
+            res += item.weight;
         });
         return res;
     };
 
-   $scope.addMoveTo = function(pc){
-       var advancedMoves = $dataCollector.getClass(pc.class.name).moves.advanced;
-       var availableMoves = _.filter(advancedMoves, function(move){
-           return !_.contains(pc.class.moves, move)
-       });
+    $scope.addMoveTo = function (pc) {
+        var advancedMoves = $dataCollector.getClass(pc.class.name).moves.advanced;
+        var availableMoves = _.filter(advancedMoves, function (move) {
+            return !_.contains(pc.class.moves, move)
+        });
 
-   };
-        $scope.addItemTo=function(pc){
+
+    };
+    $scope.removeMove = function (moveIndex, pc) {
+        pc.class.moves.splice(moveIndex, 1);
+    };
+
+    $scope.addItemTo = function (pc) {
         pc.inventory.push(
             {
-                name:'',
-                quantity:0,
-                weight:0
+                name: '',
+                quantity: 0,
+                weight: 0
             });
-    }
+    };
+
+    $scope.removeItem = function (itemIndex, pc) {
+        pc.inventory.splice(itemIndex, 1);
+    };
 
 
 });
--- public/javascripts/directives/editableCell.js
+++ public/javascripts/directives/editableCell.js
@@ -1,18 +1,16 @@
 /**
  * Created by Alex on 02/09/2014.
  */
-app.directive('ngEditableCell', function () {
+app.directive('ngEditable', function () {
     return {
         restrict: "A",
-        replace:true,
         scope: {
-            ngEditableCell: '=',
+            ngEditable: '=',
             type:'@'
         },
         template:
-            '<td ng-dblclick="edit=true" ng-init="edit=false" class=""><span ng-show="!edit">{{ngEditableCell}}</span>'+
-            '<input type="{{type}}" ng-blur="edit=false" ng-show="edit" ng-model=ngEditableCell>'+
-            '</td>',
+            '<span ng-show="!edit" ng-dblclick="edit=true" ng-init="edit=false">{{ngEditable}}</span>'+
+            '<input type="{{type}}" ng-blur="edit=false" ng-show="edit" ng-model=ngEditable>',
          link: function (scope, elem, attrs) {
         }
 

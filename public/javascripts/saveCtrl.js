7f546c0e288f43f7ec117920535ae69e37d64c34
gotenxds <David.limkys@gmail.com>
2015-04-06 22:30:24 +0300

    Added Node and DB

--- public/javascripts/saveCtrl.js
+++ public/javascripts/saveCtrl.js
@@ -1,9 +1,11 @@
 /**
  * Created by Alex on 25/09/14.
  */
-app.controller('saveCtrl', function ($scope, $dataCollector) {
+app.controller('saveCtrl', function ($scope, $dataCollector, $http) {
 
     $scope.download = function () {
+
+        $http.put('/save', $dataCollector.getPCs());
         var pom = document.createElement('a');
         pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify($dataCollector.getPCs())));
         pom.setAttribute('download', 'myDWSavedPCs.json');

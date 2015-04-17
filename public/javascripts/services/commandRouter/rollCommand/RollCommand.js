4cf2f97bd6bd5fde76f9de030ebb2423b914c01b
David limkys <David.limkys@gmail.com>
2015-04-17 18:35:17 +0300

    Samll update to rollCommand.

--- public/javascripts/services/commandRouter/rollCommand/RollCommand.js
+++ public/javascripts/services/commandRouter/rollCommand/RollCommand.js
@@ -3,18 +3,18 @@ app.factory('rollCommand', function(diceResolver, modifierResolver){
 });
 
 function RollCommand(resolvers){
-    var resolvers = resolvers;
     this.name = "Roll dice";
     this.prefix = "\\roll";
     this.description = "Roll a dice.";
     this.params = ['{dice}'];
 
     this.apply = function(command){
+        var initial = command ;
         resolvers.forEach(function(resolver){
             command = command.replace(resolver.regExp, resolver.func);
         });
 
-        return math.eval(command);
+        return "[" + initial +"] = " + "[" + command +"] = " + math.eval(command);
     };
 }
 

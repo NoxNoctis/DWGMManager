26d5af4c3b9f7c0e574323ab77d3c7a7d8828c64
David limkys <David.limkys@gmail.com>
2015-04-18 17:20:24 +0300

    Deleted redundant variable.

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var log = require('util').log;
 
-var conn;
-
 mongoose.connect("mongodb://dwgmadmin:qwerqwer@ds045679.mongolab.com:45679/dwgm", function(err) {
     if(!err) {
         log("We are connected to DB");

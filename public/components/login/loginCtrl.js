df4e41e82a148ceaeb98e9484e716d7b04781f2a
David limkys <David.limkys@gmail.com>
2015-04-22 17:41:43 +0300

    Added facebook login to client.

--- public/components/login/loginCtrl.js
+++ public/components/login/loginCtrl.js
@@ -1,12 +1,27 @@
-(function(){
+(function () {
 
     function loginCtrl(loginService) {
         this.user = {};
         this.remember = false;
 
-        this.login = function(){
+        this.login = function () {
             loginService.login(this.user);
-        }
+        };
+    }
+
+    loginCtrl.prototype.activate = function() {
+        initFacebookSdk();
+    };
+
+    function initFacebookSdk() {
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3&appId=854090267995598";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     }
 
     app.controller('loginCtrl', loginCtrl);

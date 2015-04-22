a1a895604384b8e80b0de7698abfec5e2bf08c18
David limkys <David.limkys@gmail.com>
2015-04-22 20:05:26 +0300

    Fixed callbackurl for facebook on prod.

--- server/passport/facebookStrategy.js
+++ server/passport/facebookStrategy.js
@@ -6,7 +6,7 @@ function defineLoginLogic() {
     passport.use(new FacebookStrategy({
             clientID: 854090267995598,
             clientSecret: '81e622d45089b43d89d9caa7486628f7',
-            callbackURL: "http://local.dwgm.herokuapp.com:3000/auth/facebook/callback"
+            callbackURL: "http://dwgm.herokuapp.com/auth/facebook/callback"
         },
         function (accessToken, refreshToken, profile, done) {
             User.findOrCreate({facebook_id: profile.id}, {username: profile.displayName}, function (err, user) {
